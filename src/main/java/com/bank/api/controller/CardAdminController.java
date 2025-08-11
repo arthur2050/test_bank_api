fd191917a5bddbe5c3c53e444b96482f367224c1
arthur.patsanovskiy <arthur.patsanovskiy@gmail.com>
2025-08-11 22:23:57 +0300

    Added the card admin controller.

---
+++ src/main/java/com/bank/api/controller/CardAdminController.java
@@ -0,0 +1,49 @@
+package com.bank.api.controller;
+
+import com.bank.api.dto.CardDto;
+import com.bank.api.service.CardService;
+import com.bank.api.service.CardServiceInterface;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class CardAdminController {
+
+    private final CardServiceInterface cardService;
+
+    public CardAdminController(CardServiceInterface cardService) {
+        this.cardService = cardService;
+    }
+
+    // Получить все карты
+    @GetMapping("/cards")
+    public List<CardDto> getAllCards() {
+        return cardService.getAllCards();
+    }
+
+    // Создать карту для пользователя
+    @PostMapping("/card")
+    public CardDto createCardForUser(@RequestParam String username, @RequestBody CardDto cardDto) {
+        return cardService.createCardForUser(username, cardDto);
+    }
+
+    // Блокировка/активация карты
+    @PatchMapping("/card/{cardId}/block")
+    public void blockCard(@PathVariable Long cardId) {
+        cardService.blockCard(cardId);
+    }
+
+    @PatchMapping("/card/{cardId}/activate")
+    public void activateCard(@PathVariable Long cardId) {
+        cardService.activateCard(cardId);
+    }
+
+    // Удаление карты
+    @DeleteMapping("/card/{cardId}")
+    public void deleteCard(@PathVariable Long cardId) {
+        cardService.deleteCard(cardId);
+    }
+
+}
