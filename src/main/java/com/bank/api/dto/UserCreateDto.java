1f4a1cba42dd3f68e6f6e97d8706864983adbc03
arthur.patsanovskiy <arthur.patsanovskiy@gmail.com>
2025-08-11 22:27:23 +0300

    Added the user create dto.

---
+++ src/main/java/com/bank/api/dto/UserCreateDto.java
@@ -0,0 +1,37 @@
+package com.bank.api.dto;
+
+import com.bank.api.entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+public class UserCreateDto {
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Геттеры и сеттеры
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
