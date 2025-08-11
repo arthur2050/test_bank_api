9ef37924474478904d5e2e7b3eaa793523425da9
arthur.patsanovskiy <arthur.patsanovskiy@gmail.com>
2025-08-11 22:25:56 +0300

    Added the custom detail and details service.

---
+++ src/main/java/com/bank/api/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.bank.api.service;
+
+import com.bank.api.entity.User;
+import com.bank.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
